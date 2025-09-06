8d5ecd0f2c41fa24f23f35aae2a158928ea0d9b2
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-06 13:06:46 +0600

    Footer added

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
+import Footer from "../components/Footer";
 
 const Root = () => {
     return (
-        <div className="font-montserrat w-10/11 mx-auto">
-            <HomeLayout></HomeLayout>
-            <Outlet></Outlet>
+        <div>
+            <div className="font-montserrat w-10/11 mx-auto">
+                <HomeLayout></HomeLayout>
+                <Outlet></Outlet>
+            </div>
+                <Footer></Footer>
         </div>
     );
 };
